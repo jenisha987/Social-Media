ad674c5818b0eb1ec5def619506dd7bd80407d1b
jenisha987 <jensastha987@gmail.com>
2024-05-04 17:05:08 +0545

    Adding user credentials

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('post-details/<str:pk>', views.postdetails, name="postdetails"),
     path('create-post/', views.createPost, name="createpost"),
     path('update-post/<str:pk>', views.updatePost, name="updatepost"),
-    path('delete-post/<str:pk>', views.deletePost, name="deletepost")
+    path('delete-post/<str:pk>', views.deletePost, name="deletepost"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register")
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
@@ -16,6 +22,7 @@ def postdetails(request, pk):
     return render(request, "postdetails.html", context)
 
 
+@login_required(login_url='login')
 def createPost(request):
     form = PostForm()
     if request.method == "POST":
@@ -27,6 +34,7 @@ def createPost(request):
     return render(request, 'post_form.html', context)
 
 
+@login_required(login_url='login')
 def updatePost(request, pk):
     postdetails = Post.objects.get(id=pk)
     form = PostForm(instance=postdetails)
@@ -41,6 +49,7 @@ def updatePost(request, pk):
     return render(request, 'post_form.html', context)
 
 
+@login_required(login_url='login')
 def deletePost(request, pk):
     post = Post.objects.get(id=pk)
 
@@ -49,5 +58,54 @@ def deletePost(request, pk):
         return redirect('home')
     context = {'obj':post}
     return render(request, 'delete.html')
+
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:   #user exists garxa vani login
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exists.')
+
+
+    context = {'page': page}
+    return render(request, 'login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def registerUser(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+    return render(request, 'login_register.html', {'form':form})
+
 
 
